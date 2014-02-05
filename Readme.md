7acc3e84bf1fcc11429fbe05ad5eb3cee56f94df
Lance Pollard <lancejpollard@gmail.com>
2014-02-05 00:52:45 -0800

    init

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Molecules
+
+_status: not started_
+
+Complete list of molecules from PubMed.
+
+## Resources
+
+- ftp://ftp.ncbi.nlm.nih.gov/pubchem/
+- https://pubchem.ncbi.nlm.nih.gov/docs/subcmpd_summary_page_help.html
+
+## Licence
+
+MIT
---
+++ index.js
@@ -0,0 +1,24 @@
+
+/**
+ * Module dependencies.
+ */
+
+// var fs = require('fs');
+
+/**
+ * Expose `molecules`.
+ */
+
+module.exports = molecules;
+
+/**
+ * A one-liner.
+ *
+ * @param {String} name
+ * @return {Function}
+ * @api public
+ */
+
+function molecules(name) {
+
+}
